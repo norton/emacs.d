1395789097a7eb38575c32b04dafbaba62ea4383
Vsevolod Romashov <7@7vn.ru>
2015-05-19 17:36:04 +0300

    multiple-cursors

--- init.el
+++ init.el
@@ -163,3 +163,8 @@
 
 (global-set-key (kbd "s-r") 'ace-window)
 (setq aw-dispatch-always 't)
+
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
