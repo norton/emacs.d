47154ff199980474cd93ae24b8a4fbed80e5825a
Vsevolod Romashov <7@7vn.ru>
2015-07-14 19:35:57 +0300

    scrolling by one line

--- init.el
+++ init.el
@@ -183,3 +183,9 @@
 
 (require 'helm-projectile)
 (helm-projectile-on)
+
+;;keep cursor at same position when scrolling
+(setq scroll-preserve-screen-position 1)
+;;scroll window up/down by one line
+(global-set-key (kbd "M-n") (kbd "C-u 1 C-v"))
+(global-set-key (kbd "M-p") (kbd "C-u 1 M-v"))
