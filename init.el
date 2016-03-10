67056d61f6ea76c7b06dead0fff164a54bed9b50
Vsevolod Romashov <7@7vn.ru>
2016-03-11 01:32:00 +0300

    add shrink-whitespace

--- init.el
+++ init.el
@@ -210,3 +210,5 @@
 ;;scroll window up/down by one line
 (global-set-key (kbd "M-n") (kbd "C-u 1 C-v"))
 (global-set-key (kbd "M-p") (kbd "C-u 1 M-v"))
+
+(global-set-key (kbd "M-\\") 'shrink-whitespace)
