7aa4e6b798771aa8d804f02f5ce8207070ccfbc1
Vsevolod Romashov <7@7vn.ru>
2016-07-04 17:06:58 +0300

    turn on yas-minor-mode

--- customizations/setup-clojure.el
+++ customizations/setup-clojure.el
@@ -29,7 +29,7 @@
 
 (defun my-clojure-mode-hook ()
   (clj-refactor-mode 1)
-  ;; (yas-minor-mode 1) ; for adding require/use/import statements
+  (yas-minor-mode 1) ; for adding require/use/import statements
   ;; This choice of keybinding leaves cider-macroexpand-1 unbound
   (cljr-add-keybindings-with-prefix "C-c C-m"))
 
